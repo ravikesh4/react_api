6b688750f848c7a60aa0833d6604efc4b1211fda
Ravikesh Yadav <ravikesh@netzary.com>
2020-03-26 15:14:57 +0530

    react with data retrive from api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
+import {Loading} from './Loading'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    // state 
+    this.state = {
+      users: [],
+      loading: false
+    }
+    // bind 
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  // api call 
+  
+  getUsers() {
+    this.setState({
+      loading: true
+    })
+    axios('https://api.randomuser.me/?nat=US&results=5')
+      // .then(response => console.log(response))
+      .then(response => this.setState(
+        {
+          users: [...this.state.users, ...response.data.results],
+          loading: false
+        }
+      ));
+  }
+  
+  componentWillMount() {
+    this.getUsers();
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.getUsers();
+    console.log('More User loaded');
+  }
+
+  
+  render() {
+
+    const { loading, users } = this.state;
+
+    return (
+      <div className="App" >
+        <form onSubmit={this.handleSubmit}>
+              <input type="submit" value="load user" />
+            </form>
+            <hr />
+        { !loading ? users.map(user => (
+            <div key={user.id.value}>
+            <h3 style={{ color: 'red'}}>{user.name.first}  {user.name.last}</h3> 
+            <p>{user.email}</p> 
+            <hr />
+            
+            </div>
+          )) : (<Loading message="Please wait" />)} 
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Loading.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Loading = ({message}) => {
+    return(
+        <h2>{message}</h2>
+    )
+}
